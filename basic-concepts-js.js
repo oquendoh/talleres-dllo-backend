5057360e9364b9a7900334662911a05e906ed566
oquendoh <62366597+oquendoh@users.noreply.github.com>
2024-08-09 18:29:22 -0500

    Update and rename taller1.js to basic-concepts-js.js

---
+++ basic-concepts-js.js
@@ -0,0 +1,23 @@
+//Basic js
+
+/*
+- Se puede usar o no el ;
+ */
+
+//Declarar e inicializar variable
+let variable1 = 1
+//Debugging
+console.log(typeof variable1) //"number"
+
+let variable2 = "1"
+console.log(typeof variable2) //"string"
+
+//Declarar una constante (no va a cambiar a lo largo del programa)
+const constante = 1
+
+//Funciones (funcionas como variables)
+function suma(a, b) {
+    return a + b
+}
+
+console.log(suma(1, 2)) //3
