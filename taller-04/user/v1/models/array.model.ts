5ed619ef296d1a16c6cbc4045011fb4d0e26f56f
oquendoh <62366597+oquendoh@users.noreply.github.com>
2024-11-09 08:01:56 -0500

    Create array.model.ts

---
+++ taller-04/user/v1/models/array.model.ts
@@ -0,0 +1,11 @@
+type UserArrayType = {
+	id: number;
+	name: string;
+	carrera: string;
+};
+
+const UsersArrayModel = [
+	{ id: 1, name: 'Robin Restrepo', carrera: 'Psicologia' },
+];
+
+export { UsersArrayModel, UserArrayType };
